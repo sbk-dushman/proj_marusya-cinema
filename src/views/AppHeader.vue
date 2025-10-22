06989a3d8173843972e5b5da94d83d60300daf23
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-10-22 13:23:11 +0500

    21.10 sprite part1

--- src/views/AppHeader.vue
+++ src/views/AppHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <header class="header">
-    <div class="header__logo">лого</div>
+    <div class="header__logo">
+      <svg class="icon">
+        <use xlink:href="@/assets/sprite.svg#logo_w"></use>
+      </svg>
+    </div>
     <nav class="header__nav">
       <ul class="header__nav-list">
         <li><a href="#">Главная</a></li>
