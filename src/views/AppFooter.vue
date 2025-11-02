6a0f2090d1749ee4717d3e60e3551cc56ed9a558
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2025-11-02 23:17:01 +0500

    start css base

--- src/views/AppFooter.vue
+++ src/views/AppFooter.vue
@@ -4,15 +4,39 @@
       <li>
         <a href="http://">
           <svg class="icon">
-            <use xlink:href="@/assets/sprite.svg#logo_w"></use>
+            <use class="icon" xlink:href="@/assets/sprite.svg#vk-icon"></use>
           </svg>
         </a>
       </li>
-      <li><a href="http://"> </a></li>
-      <li><a href="http://"> </a></li>
-      <li><a href="http://"> </a></li>
+      <li>
+        <a href="http://">
+          <svg class="icon">
+            <use xlink:href="@/assets/sprite.svg#yt-icon"></use>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="http://"
+          ><svg class="icon">
+            <use xlink:href="@/assets/sprite.svg#ok-icon"></use>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <a href="http://">
+          <svg class="icon">
+            <use xlink:href="@/assets/sprite.svg#tg_footer"></use>
+          </svg>
+        </a>
+      </li>
     </ul>
   </footer>
 </template>
 
-<style></style>
+<style>
+.icon {
+  fill: none;
+  width: 36px;
+  height: 36px;
+}
+</style>
--- src/views/AppHeader.vue
+++ src/views/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <div class="header__logo">
-      <svg class="icon">
+      <svg class="">
         <use xlink:href="@/assets/sprite.svg#logo_w"></use>
       </svg>
     </div>
