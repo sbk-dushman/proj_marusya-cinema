9e7d45e3e3a1ad9226408997f7011835eca238a8
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2026-08-06 00:08:06 +0500

    tabs 100%

--- src/views/AppAccount.vue
+++ src/views/AppAccount.vue
@@ -3,47 +3,42 @@
     <h2 class="section-title">Мой аккаунт</h2>
 
     <ul class="account-section__tabs tabs">
-      <button class="tabs__favorite" data-tabs-path="favorite">
+      <button class="tabs__favorite" data-tabs-path="favorite" @click="currentTab = tabs.favorite">
         <svg><use xlink:href="@/assets/sprite.svg#heart-icon"></use></svg> Избранные фильм
       </button>
-      <button class="tabs__settings active-tab" data-tabs-path="profile">
+      <button class="tabs__settings active-tab" data-tabs-path="profile" @click="currentTab = tabs.profile">
         <svg><use xlink:href="@/assets/sprite.svg#person-icon"></use></svg> Настройка аккаунта
       </button>
     </ul>
-    <component :is="AppProfile" class="tab-content"></component>
+    <component :is="currentTab.component" class="tab-content" v-bind="currentTab.props"></component>
 
     <button class="account-section__button">Выйти из аккаунта</button>
   </section>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import AppProfile from '@/components/AppProfile.vue'
+import AppList from '../components/AppList.vue'
 
-// import { useGanresStore } from '@/stores/ganreStore'
-// import AppList from '../components/AppList.vue'
-// import AppProfile from '../components/AppProfile.vue'
-// import type { TopMovieList } from '@/types'
-// import type { PropType } from 'vue'
-// const ganreStore = useGanresStore()
-// Todo prop + model связь
-//play.vuejs.org/#eNqNVdFu2jAU/RUre6CVCLRrtYcMqnVTpW0PW7X1rZmm4NyAS2JHtsOoEP++Y5uEoJYKkJB97/HxOTc3l010W9ejVUNREk0M16K2zJBt6ptUiqpW2rKvqiJWaFWxwWjsNg4+6NL3yljT5f3uEHCr+UKs9hS7/SFowzQVbLsDhRRLJVfSWMYbrUnah2zGpg53NnAyBuepbBE2mxnkNjjk9Q7dwmvxq92VqdymcjIONmEQG0tVXWaWsGNskosV42VmzDSNcqpUGvk4MrPGWoUDboPPKi6UBujs79Ddfc6E9BrSqIMkS3oGAtF+sGV/HCARB9bBEPYzbqEwOfA6nTpStv3TY/jES8GXYDgoSv+WnWTGNptwHKa9h3G4zhm32HKF0ktQsEQ4SU7/454Vl3a18PQ3k3F3JJRrjHphNRn3qoitsc+lW45cCcMzKZS0cZFVonxOmMmkiQ1pUXx0uZnSOemEXdZrZlQpcvaOiHqpWGe5aEzC3tdrH66zPBdyjsAFzlzhx4erTM+FjK2qQUZVPzZTcF4l7LrFNrgfGkriNmESnnxUrUgXpfoXrxOWNVYhiOLByf5hBT+dgg8QcNmS7uRCQFxSYTvdVy/zWswXxwAHpeCc+xQejFFI1UpISzrAM76ca9XIPGHvigv3fc10DLp+3N/dhWGwZy9ZuBoEkwf0dOG+L/Cj0LcnHNhBjjvsarqrZ3hXfS9Fw6gdPG5OHX9pQ6OGfvdzq+vYIw0L5m5knU4dZt4J3L1Zdzp7OzBP4LcG868Q89GTURI3+BpjNuCkKEn/rK3AfEyjJFTf5bISDf7dx6xuwqT0ZxbEl6/En8zaxdLoXhNeGkzRqMtZNBTZkL77/YPWWHfJSuVNCfQbyV+EHmicxgD7jNaB7B7Oq/3m/yPQGw/mbm1JmtaUE+qQW49PI5T5yxvW93KvRtf+HLos2v4HdCJYbw==
-//vuejs.org/guide/essentials/component-basics
+const topMoveieStore = {
+  getTopMoveie: [],
+}
 
-// const tabs = {
-//   AppList,
-//   AppProfile,
-// }
-// const currentTab = defineModel('currentTab', {
-//   default: { cuerentName: 'AppList', data: useGanresStore().ganreList },
-// })
-// function changeTab(currentTab) {
-//   return currentTab
-// }
-// defineProps({
-//   curentTab: {
-//     type: String,
-//     default: 'none',
-//   },
-// })
+const tabs = {
+  favorite: {
+    component: AppList,
+    props: {
+      'list-type': '2',
+      list: topMoveieStore.getTopMoveie,
+    },
+  },
+  profile: {
+    component: AppProfile,
+    props: {},
+  },
+}
+
+const currentTab = ref(tabs.profile)
 </script>
 <style>
 .account-section__tabs {
