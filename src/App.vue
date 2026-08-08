9fccfbb8239848488fe6defa17bcf5dd27daf17e
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2026-08-08 23:51:03 +0500

    start of modal auth

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <template>
+  <AppModal v-show="modalOpen" />
+
   <AppHeader />
   <main>
     <RouterView />
@@ -9,5 +11,13 @@
 import { RouterView } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
 import AppFooter from './views/AppFooter.vue'
+import AppModal from '@/views/AppModal.vue'
+defineProps({
+  modalOpen: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
+
 <style scoped></style>
